9dbaf2fe40d766f321a6e309cdcf6eedfae19548
Dan Trusca <truscadani@yahoo.com>
2020-10-29 19:57:42 +0200

    create a profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-// @route GET api/profile
-// @desc Test route
-// @access PUBLIC
-router.get("/", (req, res) => {
-  res.send("Profile routes");
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route GET api/profile/me
+// @desc get the current user profile
+// @access PRIVATE
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
